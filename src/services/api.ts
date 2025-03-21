d18d7c544984f07bcc7903b54407dc6669188eda
Caio Zanqueta <caiozan2001@hotmail.com>
2025-03-21 19:14:51 -0300

    Update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'http://localhost:3333'
-  baseURL: 'http://192.168.18.142:3333'
+  baseURL: 'https://pizzaria-backend-jet.vercel.app/' //online
+  // baseURL: 'http://192.168.18.142:3333' //notebook
+  // baseURL: 'http://192.168.18.78:3333' //pc
 })
 
 export { api }
